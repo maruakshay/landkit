2dfe80465faaa3bc46448208abc2a3d82f2b2410
Akshay Maru <akshaymaru@Urami.local>
2021-07-29 14:10:01 +0530

    changes

--- README.md
+++ README.md
@@ -3,5 +3,14 @@ Hello Reader,
 
 I have made a replica of the project you send me. There are few styling left but i have made react based app/ site. 
 
+Workings :
 
-Thank you 
+in the form i have made a download sample .. write name mail and password , when click the button will say no and then no data can be stored after. the user is stored in the array and made json so that it can travel to the front end and backend. 
+
+Tried to make it responsive and added animations , most of it are custom css through but i wanted to add a handy animation so used animate.css
+
+There are components which uses props to define the data 
+
+
+
+WISH YOU A HAPPY DAY ........!
--- src/components/Form.js
+++ src/components/Form.js
@@ -6,17 +6,30 @@ export default () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [download, setDownload] = useState(true);
+    const [disable, setDisable] = useState(false);
+    const [data , setData] = useState([]);
+    const user = {
+        name : '',
+        email: '',
+        password: ''
+    }
     const handleClick = (e) => {
         e.preventDefault();
 
         if(name === '' || email === '' || password === '')
         console.log('you havent entered data');
         else{
-            console.log(name, email, password);
+            user.name = name;
+            user.email = email;
+            user.password = password;
+            data.push(user);
+            console.log(data);
             setName('');
             setEmail('');
             setPassword('');
+            setDisable(prevState => !prevState);
             setDownload(prevState => !prevState);
+           
         }
        
     }
@@ -31,7 +44,7 @@ export default () => {
                         <input text placeholder='Name' onChange={e => setName(e.target.value)} value={name}/>
                         <input text placeholder='Email' onChange={e => setEmail(e.target.value)} value={email}/>
                         <input text placeholder='Password' onChange={e => setPassword(e.target.value)} value ={password}/>
-                        <button className="btn" onClick={handleClick}>{download? 'Download a Sample' : 'Sample Downloaded'}</button>
+                        <button className="btn" style={disable?{cursor:'not-allowed'}: {}}  onClick={handleClick}>{download? 'Download a Sample' : 'Sample Downloaded'}</button>
 
                     </div>
                 </div>
